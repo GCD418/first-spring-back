e2c3efa1504ea12587e09b38433c867ec2ca8346
gabriel <patosquare418@gmail.com>
2026-02-26 09:39:16 -0400

    feat: repository looks awesome

---
+++ src/main/java/arch/linux/firstback/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package arch.linux.firstback.repository;
+
+import arch.linux.firstback.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
