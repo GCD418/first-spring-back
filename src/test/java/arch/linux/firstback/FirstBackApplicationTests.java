2aae7759ff7195ca1a91f64f12a5f3400d67908f
gabriel <patosquare418@gmail.com>
2026-02-26 09:28:07 -0400

    feat: first commit

---
+++ src/test/java/arch/linux/firstback/FirstBackApplicationTests.java
@@ -0,0 +1,13 @@
+package arch.linux.firstback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
