9632a7567369ecadb553f29b99b879e821d6cf7a
gabriel <patosquare418@gmail.com>
2026-02-26 09:48:04 -0400

    feat: employee controller

---
+++ src/main/java/arch/linux/firstback/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package arch.linux.firstback.controller;
+
+import arch.linux.firstback.model.Employee;
+import arch.linux.firstback.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //GetAll
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
