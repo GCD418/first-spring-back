42de5529ec363d1610c72f9e563da1b4d8d771ef
gabriel <patosquare418@gmail.com>
2026-02-26 15:12:03 -0400

    feat: create endpoint

--- src/main/java/arch/linux/firstback/controller/EmployeeController.java
+++ src/main/java/arch/linux/firstback/controller/EmployeeController.java
@@ -3,12 +3,11 @@ package arch.linux.firstback.controller;
 import arch.linux.firstback.model.Employee;
 import arch.linux.firstback.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -19,5 +18,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    //Create
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 }
